691be83126a69a342c26bdaaa2144c04a1f20d07
Tomáš <kretom84@gmail.com>
2021-04-13 23:14:19 +0200

    Small update #8

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -2,11 +2,33 @@ import React, { useRef, useState, useEffect } from 'react';
 import { useTransition, animated, config } from 'react-spring';
 
 import Video from "../images/bgVideo1.mp4";
+import Image from "../images/slide1.jpg";
 
 const Slider = () =>{
 
   const videoRef = useRef(null);
+
+  const [index, setIndex] = useState(0);
   const [showHeader, setShowHeader] = useState(false);
+  const [isDesktop, setDesktop] = useState(window.innerWidth >= 768);
+
+  const updateMedia = () => {
+    setDesktop(window.innerWidth >= 768);
+  };
+
+  const texts = [
+    {id: 0, text: "Chiptuning osobních aut"},
+    {id: 1, text: "Chiptuning nákladních aut"},
+    {id: 2, text: "Chiptuning zemědělské techniky"},
+  ];
+
+  const textTransitions = useTransition(showHeader ? texts[index] : [], {
+    from: { opacity: 0, transform: "translate3d(0,-15%,0) scale3d(1,1.1,1)" },
+    enter: { opacity: 1, transform: "translate3d(0,0,0) scale3d(1,1,1)" },
+    leave: { opacity: 0, transform: "translate3d(0,15%,0) scale3d(1,0.95,1)" },
+    config: config.gentle,
+    key: texts[index].id,
+  });
 
   useEffect(() => {
     const headerInterval = setInterval(() => setShowHeader(true), 500);
@@ -15,9 +37,11 @@ const Slider = () =>{
   }, []);
 
   useEffect(() => {
-    const { current: videoElement } = videoRef
-    videoElement.setAttribute('muted', '')
-  }, []);
+    if(isDesktop){
+      const { current: videoElement } = videoRef
+      videoElement.setAttribute('muted', '')
+    }
+  }, [isDesktop]);
 
   useEffect(() => {
     const slideInterval = setInterval(() => {
@@ -28,21 +52,10 @@ const Slider = () =>{
 
   }, []);
 
-  const texts = [
-    {id: 0, text: "Chiptuning osobních aut"},
-    {id: 1, text: "Chiptuning nákladních aut"},
-    {id: 2, text: "Chiptuning zemědělské techniky"},
-  ];
-
-  const [index, setIndex] = useState(0);
-
-  const textTransitions = useTransition(showHeader ? texts[index] : [], {
-    from: { opacity: 0, transform: "translate3d(0,-15%,0) scale3d(1,1.1,1)" },
-    enter: { opacity: 1, transform: "translate3d(0,0,0) scale3d(1,1,1)" },
-    leave: { opacity: 0, transform: "translate3d(0,15%,0) scale3d(1,0.95,1)" },
-    config: config.gentle,
-    key: texts[index].id,
-  });
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, []);
 
   return(
     <div className="sliderContainer">
@@ -58,7 +71,11 @@ const Slider = () =>{
 
       <div className="sliderVideo">
 
-        <video src={Video} ref={videoRef} playsInline autoPlay loop disablePictureInPicture muted />
+      {
+        isDesktop
+        ?  <video src={Video} ref={videoRef} playsInline autoPlay loop disablePictureInPicture muted />
+        :  <img src={Image} alt="Chiptuning Brno Banner" />
+      }
 
       </div>
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
-import { hydrate, render } from "react-dom";
+import reportWebVitals from "./reportWebVitals";
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+
+function sendToGoogleAnalytics({name, delta, value, id}) {
+  window.gtag('event', name, {
+    // Built-in params:
+    value: delta,
+    // Custom params:
+    metric_id: id,
+    metric_value: value,
+    metric_delta: delta,
+  });
 }
+
+reportWebVitals(sendToGoogleAnalytics);
