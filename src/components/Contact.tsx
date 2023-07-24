be199ae64cce677011522ce3d4d9aa876938b830
Tomáš Kretek <kretom84@gmail.com>
2023-07-24 18:40:16 +0200

    Contact fix

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,7 @@ import { useInView } from 'react-intersection-observer'
 
 const Contact = () => {
   const map1 =
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1304.8064512927522!2d16.874747626308054!3d49.150973647621946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4712e9be1edda5b5%3A0x2abfb1928c2aec2d!2sQUANTUM%20-%20Autorizovan%C3%BD%20chiptuning%20-%20osobn%C3%AD%20vozidla%20%2F%20dod%C3%A1vky!5e0!3m2!1scs!2scz!4v1620913917290!5m2!1scs!2scz'
+    'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1304.8051330439973!2d16.8751005!3d49.1510237!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4712e9be1edda5b5%3A0x2abfb1928c2aec2d!2sQUANTUM%20-%20Autorizovan%C3%BD%20chiptuning%20-%20osobn%C3%AD%20vozidla%20%2F%20dod%C3%A1vky!5e0!3m2!1scs!2scz!4v1690215012086!5m2!1scs!2scz'
   const map2 =
     'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2194.7753565609282!2d16.87918378857249!3d49.14281805683453!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x753fb91edeaff950!2sQUANTUM%20-%20Autorizovan%C3%BD%20chiptuning%20Brno%20%E2%80%93%20kamiony%2Fautobusy%2Fagro!5e0!3m2!1scs!2scz!4v1619280422227!5m2!1scs!2scz'
 
@@ -34,7 +34,7 @@ const Contact = () => {
           <ul>
             <li className="bold">Adresa</li>
             <li>Quantum cars Brno</li>
-            <li>Československé armády 1213</li>
+            <li>Československé armády 2969</li>
             <li>684 01 Slavkov u&nbsp;Brna</li>
             <li className="bold">Mobil</li>
             <li>+420 777 227 767</li>
@@ -55,7 +55,7 @@ const Contact = () => {
           <ul>
             <li className="bold">Adresa</li>
             <li>Quantum trucks Brno</li>
-            <li>Československé armády 1213</li>
+            <li>Československé armády 2969</li>
             <li>684 01 Slavkov u&nbsp;Brna</li>
             <li className="bold">Mobil</li>
             <li>+420 777 227 767</li>
